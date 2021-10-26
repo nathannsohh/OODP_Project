28e68c180eda88a2eb28bd64ca1fceb4aca77e75
Nathan <nathansoh@gmail.com>
2021-10-26 12:25:52 +0800

    -

--- Table.java
+++ Table.java
@@ -3,6 +3,30 @@ public class Table {
 	private int tableNumber;
 	private int capacity;
 	private boolean available;
-	private int attribute;
 
+	public Table(int tableNumber, int capacity, boolean available){
+		this.tableNumber = tableNumber;
+		this.capacity = capacity;
+		this.available = available;
+	}
+
+	public int getTableNumber() {
+		return tableNumber;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public boolean getAvailable() {
+		return available;
+	}
+
+	public void setTableNumber(int tableNumber) {
+		this.tableNumber = tableNumber;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
 }
--- TableManager.java
+++ TableManager.java
@@ -1,10 +1,19 @@
 public class TableManager {
 
-	private List<Table> tables;
+	private Table[] tables;
 
-	public void checkAvailability() {
-		// TODO - implement TableManager.checkAvailability
-		throw new UnsupportedOperationException();
+	public TableManager(){
+		tables = new Table[10];
+		for(int i = 0; i < 10; i++){
+			if(i%5==0) tables[i] = new Table(i+1, 2, true);
+			else if(i%5==1) tables[i] = new Table(i+1, 4, true);
+			else if(i%5==2) tables[i] = new Table(i+1, 6, true);
+			else if(i%5==3) tables[i] = new Table(i+1, 8, true);
+			else if(i%5==4) tables[i] = new Table(i+1, 10, true);
+		}
 	}
 
+	public boolean checkAvailability(int tableNumber) {
+		return tables[tableNumber-1].getAvailable();
+	}
 }
