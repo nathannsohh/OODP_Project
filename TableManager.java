c5ec33487a9ef63d173d31a41b89ec612128830f
Nathan <nathansoh@gmail.com>
2021-11-01 15:33:05 +0800

    -added getTables -changed the way tables are initialised (by size) -changed the name for the 2 checkAvailability methods and both returns tablenumber -added isValidTableNumber -added setTableAvailability

--- TableManager.java
+++ TableManager.java
@@ -1,35 +1,57 @@
-import java.util.Date;
-import java.util.List;
+import java.util.ArrayList;
+import java.time.*;
 
 public class TableManager {
 
 	private Table[] tables;
-
+	
 	public TableManager(){ //use txt file to get tables
 		tables = new Table[10];
 		for(int i = 0; i < 10; i++){
-			if(i%5==0) tables[i] = new Table(i+1, 2, true);
-			else if(i%5==1) tables[i] = new Table(i+1, 4, true);
-			else if(i%5==2) tables[i] = new Table(i+1, 6, true);
-			else if(i%5==3) tables[i] = new Table(i+1, 8, true);
-			else if(i%5==4) tables[i] = new Table(i+1, 10, true);
+			if(i==0||i==1) tables[i] = new Table(i+1, 2, true);
+			else if(i==2||i==3) tables[i] = new Table(i+1, 4, true);
+			else if(i==4||i==5) tables[i] = new Table(i+1, 6, true);
+			else if(i==6||i==7) tables[i] = new Table(i+1, 8, true);
+			else if(i==8||i==9) tables[i] = new Table(i+1, 10, true);
 		}
-
-
-	public int checkAvailability(int pax, Date datetime, ReservationManager manager) { // return table num
-		// TODO - implement TableManager.checkAvailability
-		throw new UnsupportedOperationException();
-	}
-	public int checkAvailability(int pax) { // return table num
-		// TODO - implement TableManager.checkAvailability
-		throw new UnsupportedOperationException();
 	}
 
-	public boolean checkAvailability(int tableNumber) {
-		return tables[tableNumber-1].getAvailable();
+	public Table[] getTables(){
+		return tables;
 	}
+
+
+	public int checkFutureAvailability(int pax, LocalDateTime datetime, ReservationManager manager) { 
+		ArrayList<Reservation> reservationList = manager.getReservationList();
+		ArrayList<Integer> tableNumList = new ArrayList<Integer>();
+		for(int i = 0; i < reservationList.size(); i++){
+			if(reservationList.get(i).getDatetime() == datetime) {
+				tableNumList.add(reservationList.get(i).getTableNumber());  // get list of tables that are reserved at datetime
+			}
+		}
+		for(int i = 0; i < 10; i++){
+			if(tableNumList.contains(i+1)){
+				continue;
+			}
+			else {
+				if(tables[i].getCapacity() >= pax) return tables[i].getTableNumber();
+			}
+		}
+		return -1;
+	}
+	public int checkCurrentAvailability(int pax) { 
+		for(int i = 0; i < 10; i++){
+			if(tables[i].getCapacity() >= pax && tables[i].getAvailable() == true) return tables[i].getTableNumber();
+		}
+		return -1;
+	}
+
 	public boolean isValidTableNumber(int tableNumber) {
 		if(tableNumber > 10 || tableNumber < 1) return false;
 		else return true;
+	}
+
+	public void setTableAvailability(int tableNumber, boolean avail){
+		tables[tableNumber-1].setAvailable(avail);
 	}
 }
