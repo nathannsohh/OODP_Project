4cbd91a629ac524dbfa4c9fd9c04e68ba06a1c7d
gregoryong16 <gregory.ong16@gmail.com>
2021-11-01 15:43:17 +0800

    Did i commit correctly this time?

--- Order.java
+++ Order.java
@@ -5,58 +5,45 @@ import java.time.format.DateTimeFormatter;
 
 public class Order {
 
-	//private Staff staff;
+	private Staff staff;
 	private ArrayList<OrderItem> order;
-	private LocalDateTime datetime;
-	private float price;
+	private Date datetime;
 	private float subtotal;
 	private float priceBefGST;
-	private int OrderID;
-	private Table table;
+	private int tableNumber;
 	private Customer customer;
 
 
-	public Order(Staff staff,String datetime,int OrderID,Table table, Customer customer){
+	public Order(Staff staff,Date datetime,int tableNumber, Customer customer){
 		order=new ArrayList<OrderItem>();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
-		this.datetime = LocalDateTime.parse(datetime, formatter);
-		this.OrderID=OrderID;
-		this.table=table;
+		this.staff=staff;
+		this.datetime = datetime;
+		this.tableNumber=tableNumber;
 		this.customer=customer;
 	}
-
 	
+	public Date getDatetime(){
+		return this.datetime;
+	}
+	
+	public float getPriceBefGST(){
+		return priceBefGST;
+	}
+
+	public int getTableNumber(){
+		return tableNumber;
+	}
+
 	public ArrayList<OrderItem> getOrder(){
 		return this.order;
-
 	}
 	
-	public ArrayList<OrderItem> getOrder(int tableNumber){
-		if(table.getTableNumber()==tableNumber){
-			return order;
-		}
-		else{
-			return null;
-		}
-	}
-
 	public void addItem(MenuItem itemname,int quantity) {
 		OrderItem orderitem=new OrderItem(itemname,quantity);
 		order.add(orderitem);
 	}
 
-	public int getOrderID(){
-		return this.OrderID;
-	}
-
-	public LocalDateTime getDatetime(){
-		return this.datetime;
-	}
-	public float getPriceBefGST(){
-		return priceBefGST;
-	}
-
-	public void removeItem(MenuItem itemname,int quantity) {
+	public void removeItem(MenuItem itemname,int quantity) { 
 		for(int i = 0; i < order.size(); i++){
 			if(order.get(i).getMenuItem()==itemname){
 				if( (order.get(i).getQuantity()-quantity) < 0 ){
@@ -79,42 +66,23 @@ public class Order {
 		}
 	}
 	
-	public void removeItem(MenuItem itemname){
-		for(int i = 0; i < order.size(); i++){
-			if(order.get(i).getMenuItem()==itemname){
-				if(order.get(i).getQuantity()-1==0){
-					order.remove(i);
-					System.out.println("1 " + itemname+" removed!");
-				}
-				else{
-					order.get(i).setQuantity(order.get(i).getQuantity()-1);
-					System.out.println("1 " + itemname+" removed!");
-				}
-			}
-			else{
-				System.out.println("No such item in the order!");
-			}
-		}
-	}
-
+	
 	public void viewOrder() {
 		System.out.println("Quantity         Items");
 		for(int j=0;j<order.size();j++){
 				System.out.println(order.get(j).getQuantity() + "     " + order.get(j).getMenuItem().getName() + "\n");
+		}
 	}
-}
 
-	public void viewOrder() {
-		// TODO - implement OrderManager.viewOrder
-		throw new UnsupportedOperationException();
-	}
 
 	public void printOrderInvoice() {
 		float discount=0;
+		float price=0;
 		System.out.println("                     MacDonalds                        ");
 		System.out.println("*******************************************************");
-		System.out.println("Table Number: " + table.getTableNumber());
+		System.out.println("Table Number: " + tableNumber);
 		System.out.println(datetime);
+		System.out.println("Served by: "+ staff.getName());
 		System.out.println("_______________________________________________________");
 		System.out.println("Quantity      Item                            Price");
 		for(int i=0;i<order.size();i++){
