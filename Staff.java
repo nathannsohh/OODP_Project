4c5e045aae46c179bc3e01b97f540819dbf8c79c
gregoryong16 <gregory.ong16@gmail.com>
2021-11-06 21:24:22 +0800

    hi

--- Staff.java
+++ Staff.java
@@ -1,19 +1,56 @@
 import java.util.*;
 import java.util.function.Supplier;
+enum Gender{Male,Female};
+enum JobTitle{waiter,cashier,manager};
 public class Staff {
 
 	private String name;
-	enum Gender{Male,Female};
-	enum JobTitle{waiter,cashier,manager};
+	private Gender gender;
+	private JobTitle jobTitle;
+	private String employeeID;
 	
-	public Staff(String name, int gender, JobTitle jobTitle){
+	public Staff(String name, Gender gender, String employeeID, JobTitle jobTitle){
 		this.name=name;
+		this.employeeID=employeeID;
+		this.gender= gender;
+		this.jobTitle=jobTitle;
 	}
 
+	
+	/** 
+	 * @return Gender
+	 */
+	public Gender getGender(){
+		return gender;
+	}
+
+	
+	/** 
+	 * @return JobTitle
+	 */
+	public JobTitle getJobTitle(){
+		return jobTitle;
+	}
+
+	
+	/** 
+	 * @return String
+	 */
+	public String getEmployeeID(){
+		return employeeID;
+	}
+	
+	/** 
+	 * @return String
+	 */
 	public String getName(){
 		return this.name;
 	}
 	
+	
+	/** 
+	 * @param s
+	 */
 	public void setName(String s){
 		this.name=s;
 	}
